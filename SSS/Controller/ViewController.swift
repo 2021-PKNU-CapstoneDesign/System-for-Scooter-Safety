c262676b7bed5ea5997456e6da30bead45d5c43f
kang15567 <kang15567@gmail.com>
2021-11-18 13:36:01 +0900

    textView placeHolder

--- SSS/Controller/ViewController.swift
+++ SSS/Controller/ViewController.swift
@@ -7,12 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
   
+    @IBOutlet weak var reportTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        placeholderSetting()
     }
-
+    
+    //textView placeholder
+    func placeholderSetting() {
+        reportTextView.delegate = self
+        reportTextView.text = "신고내용을 작성해주세요."
+        reportTextView.textColor = UIColor.lightGray
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if reportTextView.textColor == UIColor.lightGray {
+            reportTextView.text = nil
+            reportTextView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+            if textView.text.isEmpty {
+                textView.text = "신고내용을 작성해주세요."
+                textView.textColor = UIColor.lightGray
+        }
+    }
 }
 
